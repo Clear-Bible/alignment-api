c7a0f90ad0583402916e894d1ae88fc474de9b96
Mike Brinker <themikejr@gmail.com>
2023-02-21 15:38:58 -0500

    initial endpoint working.

--- server/alignment_api/views.py
+++ server/alignment_api/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def get_alignments(request):
+    print("get_existing_alignments")
+    return HttpResponse("You've hit the existing alignments endpoint.")
