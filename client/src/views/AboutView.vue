78d84ac20c512a159f68fca837ce734435de38cf
Mike Brinker <themikejr@gmail.com>
2023-03-08 11:38:35 -0500

    client: new and improved about page.

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <p>This is a prototype built by Clear Bible. It consists of:</p>
+
+    <ol>
+      <li>
+        A
+        <a href="https://github.com/clear-Bible/Alignments"
+          >public git repository</a
+        >
+        that makes Bible alignment data available for researchers and
+        developers.
+      </li>
+      <li>
+        A
+        <a href="https://github.com/Clear-Bible/alignment-api/tree/main/server"
+          >server API</a
+        >
+        that exposes the Bible alignment data.
+      </li>
+      <li>
+        A
+        <a href="https://github.com/Clear-Bible/alignment-api/tree/main/client"
+          >client</a
+        >
+        that provides a query interface to the API.
+      </li>
+    </ol>
+
+    <p>
+      The goal of this protoype is to explored modelling alignment data and
+      building a community around it. We aim to release our software and data
+      with open licenses so that it is unencumbered for use by others. For more
+      information, visit <a href="https://clear.bible">clear.bible</a>.
+    </p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style scoped></style>
