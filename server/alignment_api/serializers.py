84ece49dc176b49752948217a84d7040b0e17238
Mike Brinker <themikejr@gmail.com>
2023-03-14 15:49:18 -0400

    server: fully replace views with DRF.

--- server/alignment_api/serializers.py
+++ server/alignment_api/serializers.py
@@ -4,9 +4,6 @@ from .models import Alignment, Link, SourceToken, TargetToken
 
 # Learn more: https://www.django-rest-framework.org/api-guide/relations/
 class AlignmentSerializer(serializers.ModelSerializer):
-    source = serializers.StringRelatedField(many=False)
-    target = serializers.StringRelatedField(many=False)
-
     class Meta:
         model = Alignment
         fields = ["id", "name", "source", "target"]
@@ -16,3 +13,20 @@ class LinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
         fields = ["alignment", "source_tokens", "target_tokens"]
+
+
+class SourceTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SourceToken
+        fields = ["id", "token_id", "resource", "text"]
+
+
+class TargetTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TargetToken
+        fields = ["id", "token_id", "resource", "text"]
+
+
+class LinkReadSerializer(LinkSerializer):
+    source_tokens = SourceTokenSerializer(read_only=True, many=True)
+    target_tokens = TargetTokenSerializer(read_only=True, many=True)
--- server/alignment_api/views.py
+++ server/alignment_api/views.py
@@ -3,34 +3,8 @@ from django.shortcuts import render
 
 from .models import Alignment, Link, SourceToken, TargetToken
 
-from .serializers import AlignmentSerializer, LinkSerializer
+from .serializers import AlignmentSerializer, LinkSerializer, LinkReadSerializer
 from rest_framework import viewsets, permissions, generics, status
-
-# Create your views here.
-
-
-def convert_tokens(tokens):
-    returned_tokens = []
-    for token in tokens:
-        returned_tokens.append(
-            {
-                "tokenId": token["token_id"],
-                "resourceId": token["resource_id"],
-                "text": token["text"],
-            }
-        )
-    return returned_tokens
-
-
-def convert_links(links):
-    converted_links = []
-    for link in links:
-        converted_link = {}
-        converted_link["id"] = link.id
-        converted_link["sourceTokens"] = convert_tokens(link.source_tokens.values())
-        converted_link["targetTokens"] = convert_tokens(link.target_tokens.values())
-        converted_links.append(converted_link)
-    return converted_links
 
 
 class AlignmentViewSet(viewsets.ModelViewSet):
@@ -40,53 +14,25 @@ class AlignmentViewSet(viewsets.ModelViewSet):
 
     queryset = Alignment.objects.all()
     serializer_class = AlignmentSerializer
+
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-def get_alignments(request):
-    alignments = Alignment.objects.all()
-    response = {}
-    response["alignments"] = []
-
-    for alignment in alignments:
-        simplified_alignment = {}
-        links = []
-
-        simplified_alignment["id"] = alignment.name
-        simplified_alignment["source"] = alignment.source.name
-        simplified_alignment["target"] = alignment.target.name
-        simplified_alignment["linkNum"] = alignment.link_set.all().count()
-
-        # simplified_alignment["links"] = convert_links(alignment.link_set.all())
-        response["alignments"].append(simplified_alignment)
-
-    return JsonResponse(response)
-
-
-def get_alignment(request, alignment_name):
-    alignment = Alignment.objects.get(name=alignment_name)
-    response = {}
-    response["id"] = alignment.name
-    response["source"] = alignment.source.name
-    response["target"] = alignment.target.name
-    response["linkNum"] = alignment.link_set.all().count()
-    return JsonResponse(response)
 
 
 class LinkList(generics.ListAPIView):
     """
     API endpoint that allows links to be viewed by alignment and scope.
+    Scope is required.
     """
 
-    serializer_class = LinkSerializer
+    def get_serializer_class(self):
+        if self.request.method in ["GET"]:
+            return LinkReadSerializer
+        return LinkSerializer
 
     def get_queryset(self):
-        print("MIKEY", self.kwargs)
         alignment_id = self.kwargs["alignment"]
         alignment = Alignment.objects.get(id=alignment_id)
-        print(f"found alignment {alignment}")
         scopes = self.request.query_params.getlist("source_token")
-        print("MIKEY", scopes)
 
         source_tokens_in_scope = []
         for scope in scopes:
@@ -95,29 +41,6 @@ class LinkList(generics.ListAPIView):
             )
             source_tokens_in_scope.extend(scoped_tokens)
 
-        print(f" num scoped: {len(source_tokens_in_scope)}")
         return Link.objects.filter(
             alignment=alignment, source_tokens__in=source_tokens_in_scope
         )
-
-
-def get_links(request, alignment_name):
-    # Return specified links or a few random ones
-    source_token_ids = request.GET.getlist("source_tokens", "")
-    alignment = Alignment.objects.get(name=alignment_name)
-
-    source_tokens = []
-    for source_token_id in source_token_ids:
-        found_tokens = SourceToken.objects.filter(
-            token_id__startswith=source_token_id, resource=alignment.source
-        )
-        source_tokens.extend(found_tokens)
-
-    links = []
-    for source_token in source_tokens:
-        links.extend(source_token.links.all())
-
-    response = {}
-    response["linkNum"] = len(links)
-    response["links"] = convert_links(links)
-    return JsonResponse(response)
